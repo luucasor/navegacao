5228f651492ab067df0428ac9a994d703e4a3506
luucasor <lucas@jornadatecnologia.com>
2023-07-27 18:03:48 -0300

    Added nav, menu and content

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import ReactDOM from 'react-dom';
 import React from 'react';
 import App from './views/App';
 
+import './index'
+
 ReactDOM.render(
     <App />,
     document.getElementById('root')
--- src/views/App.jsx
+++ src/views/App.jsx
@@ -1,8 +1,11 @@
 import './App.css'
+import Menu from '../components/layout/Menu'
+import Content from '../components/layout/Content'
 
 const App = props => (
     <div className="App">
-        Componente App
+        <Menu />
+        <Content />
     </div>
 )
 
