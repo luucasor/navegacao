9c69b7fdd8404db327f050eb24d41dd893e5acbc
luucasor <lucas@jornadatecnologia.com>
2023-07-28 11:08:54 -0300

    new corcurrent renderer

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import React from 'react';
 import App from './views/App';
-
 import './index'
 
-ReactDOM.render(
-    <App />,
-    document.getElementById('root')
-)
+const container = document.getElementById('root')
+const root = createRoot(container)
+root.render(<App />)
